7b8037fc2926bc345342625f9935bd2b851dc932
russianGay <34456574+russianGay@users.noreply.github.com>
2017-12-12 03:33:39 +0800

    Start

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+	// Links array
+	// Don't read this code, men. .!.
+	var links = new Array();
+	links[0] = 'img/dulo.jpg';
+	links[1] = 'img/dildo.jpg';
+	// Menu array
+	var menu = new Array();
+	menu = [{name: 'Новости', link: 'index.html'}]
+	// Set background from links
+	// My english is perfect, no ne ochen
+	function setBackground(number) {
+		var id = '#post-' + number;
+		$(id).css({'background': 'url(' + links[number] + ')'});
+	}
+	// Execute this function very many times
+	for (var i = 0; i < links.length; i++) {
+		setBackground(i);
+	}
+	// Set menu
+	for (var i = 0; i < menu.length; i++) {
+		$('.menu').append('<li><a class="link" href="' + menu[i].link + '">' + menu[i].name + '</a></li>');;
+	}
+});
